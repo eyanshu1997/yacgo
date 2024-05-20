19284c96a6b74b05a791fbf66152742acbfb81a0
eyanshu1997 <eyanshu1997@gmail.com>
2024-05-20 23:34:09 +0530

    update repl to use parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,15 @@ import (
 	"io"
 
 	"github.com/eyanshu1997/yacgo/lexer"
-	"github.com/eyanshu1997/yacgo/tokens"
 )
 
 const PROMPT = ">> "
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -21,8 +26,13 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.NewLexer(line)
-		for tok := l.ReadNextToken(); tok.Type != tokens.TokenTypeEOF; tok = l.ReadNextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
