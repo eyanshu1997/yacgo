0b8a459c18bb8e77efa0a442622fb53a483cd26b
eyanshu1997 <eyanshu1997@gmail.com>
2024-05-20 21:01:51 +0530

    add support for boolean and paranthesis

--- ast/expression.go
+++ ast/expression.go
@@ -6,29 +6,6 @@ import (
 	"github.com/eyanshu1997/yacgo/tokens"
 )
 
-type ExpressionStatement struct {
-	Token      tokens.Token // the first token of the expression
-	Expression Expression
-}
-
-func (es *ExpressionStatement) statementNode()       {}
-func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
-func (es *ExpressionStatement) String() string {
-	if es.Expression != nil {
-		return es.Expression.String()
-	}
-	return ""
-}
-
-type IntegerLiteral struct {
-	Token tokens.Token
-	Value int64
-}
-
-func (il *IntegerLiteral) expressionNode()      {}
-func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
-func (il *IntegerLiteral) String() string       { return il.Token.Literal }
-
 type PrefixExpression struct {
 	Token tokens.Token // The prefix token, e.g. !
 
